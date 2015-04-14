c81f4baafb1905829a13d3c6ad511dcd8dc09aed
Adam Shaylor <business@adamshaylor.com>
2015-04-14 10:59:22 -0700

    Finish build and README.md

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+
+
+	var config = {},
+		banner;
+
+
+	config.pkg = grunt.file.readJSON('package.json');
+
+
+	config.mochaTest = {
+		rsvpBacklog: {
+			options: {
+				reporter: 'spec',
+				captureFile: 'test/results.txt',
+				clearRequireCache: true
+			},
+			src: ['test/all.js']
+		}
+	};
+
+
+	config.uglify = {
+		rsvpBacklog: {
+			options: {
+				sourceMap: true
+			},
+			files: {
+				'rsvp-backlog.min.js': ['rsvp-backlog.js']
+			}
+		}
+	};
+
+
+	config.watch = {
+		rsvpBacklog: {
+			files: ['rsvp-backlog.js'],
+			tasks: ['default']
+		},
+		options: {
+			atBegin: true
+		}
+	};
+
+
+	grunt.initConfig(config);
+
+
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-notify');
+
+
+	grunt.registerTask('default', ['mochaTest', 'uglify']);
+	grunt.registerTask('dev', ['watch']);
+
+
+	grunt.task.run('notify_hooks');
+
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # rsvp-backlog
-Like RSVP.all() but a little smarter
+
+```javascript
+var backlog = RSVP.createBacklog();
+
+backlog.add(somePromise);
+
+// Access a promise in the backlog directly
+
+backlog.toArray()[0].then(doSomething);
+
+// The promise behind .whenClear() resets when the backlog size goes from zero 
+// to one, so you need at least one promise before you can call it
+
+backlog.whenClear().then(doSomethingElse);
+
+// Resolving or removing the last promise in the backlog resolves the backlog
+// as a whole
+
+backlog.remove(backlog.toArray()[0]);
+```
+
+## Bower installation
+
+From a terminal:
+
+```shell
+bower install rsvp-backlog
+```
+
+In your HTML:
+
+```html
+<script src="bower_components/rsvp-backlog/rsvp-backlog.min.js">
+```
+
+## Node installation
+
+From a terminal:
+
+```shell
+npm install rsvp-backlog
+```
+
+In you script:
+
+```javascript
+var createBacklog = require('rsvp-backlog'),
+    backlog = createBacklog();
+```
