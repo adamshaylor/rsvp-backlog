3f339600feddaf01b1300ab8553d697e7cc0634b
adamshaylor <business@adamshaylor.com>
2015-04-13 22:36:48 -0700

    Initial implementation Still needs a build

---
+++ rsvp-backlog.js
@@ -0,0 +1,101 @@
+(function rsvpBacklog (rsvp) {
+
+
+	function PromiseBacklog () {
+
+		var backlog = [],
+			clearedDeferral = rsvp.defer();
+
+		clearedDeferral.resolve();
+
+		this.add = function addToBacklog (promiseToAdd) {
+
+			var remove;
+
+			if (typeof promiseToAdd !== 'object' || typeof promiseToAdd.then !== 'function') {
+
+				throw new TypeError('rsvp-backlog: Expected a promise object');
+
+			}
+
+			remove = this.remove.bind(this, promiseToAdd);
+
+			promiseToAdd.then(remove, remove);
+
+			if (!backlog.length) {
+				clearedDeferral = rsvp.defer();
+			}
+
+			backlog.push(promiseToAdd);
+
+			return this;
+
+		};
+
+		this.remove = function removeFromBackLog (promiseToRemove) {
+
+			var indexToRemove = backlog.indexOf(promiseToRemove);
+
+			if (indexToRemove > -1) {
+
+				backlog.splice(indexToRemove, 1);
+
+				if (!backlog.length) {
+					clearedDeferral.resolve();
+				}
+
+			}
+
+			return this;
+
+		};
+
+		this.whenClear = function getClearedPromise () {
+
+			return clearedDeferral.promise;
+
+		};
+
+		this.toArray = function backlogToArray () {
+
+			return backlog.slice(0);
+
+		};
+
+	}
+
+
+	// A factory gives us the benefit of a named instance whilst avoiding (some of) the pitfalls of JavaScript
+	// constructors
+
+	function createBacklog () {
+
+		return new PromiseBacklog();
+
+	}
+
+
+	// Browser: export to namespaced global
+
+	if (typeof window !== 'undefined') {
+
+		window.RSVP.createBacklog = createBacklog;
+
+	}
+
+
+	// Node: use native modules
+
+	else if (typeof module !== 'undefined' && typeof require !== 'undefined') {
+
+		module.exports = createBacklog;
+
+	}
+	
+
+
+
+}).call(
+	this,
+	typeof window === 'undefined' && require ? require('rsvp') : window.RSVP
+);
---
+++ test/all.js
@@ -0,0 +1,144 @@
+/* jshint expr: true */
+
+var rsvp = require('rsvp'),
+	rsvpBacklog = require('../rsvp-backlog.js'),
+	should = require('should'),
+	globalContext = this;
+
+
+describe('rsvpBacklog()', function () {
+
+	var backlog = rsvpBacklog(),
+		deferreds = [];
+
+	it('should return an object', function () {
+
+		backlog.should.be.instanceOf(Object);
+
+	});
+
+	describe('#add()', function () {
+
+		it('should throw an error when not passed a promise', function () {
+
+			backlog.add.bind(globalContext, undefined).should.throw();
+
+			backlog.add.bind(globalContext, {}).should.throw();
+
+		});
+
+		it('should return the backlog object', function () {
+
+			var deferred = rsvp.defer(),
+				returnedObject = backlog.add(deferred.promise);
+
+			deferreds.push(deferred);
+
+			returnedObject.should.equal(backlog);
+
+		});
+
+	});
+
+	describe('#toArray()', function () {
+
+		it('should return an array containing the promise from the #add() test', function () {
+
+			var arrayified = backlog.toArray();
+
+			arrayified.should.be.instanceOf(Array);
+
+			arrayified[0].should.equal(deferreds[0].promise);
+
+		});
+
+	});
+
+	describe('#remove()', function () {
+
+		it('should remove a promise from the backlog if it exists and return the backlog object', function () {
+
+			var returnedObject = backlog.remove(deferreds[0].promise);
+
+			returnedObject.should.equal(backlog);
+
+			backlog.toArray().should.be.empty;
+
+		});
+
+		it('should still return the backlog object even if the passed promise doesn\'t exist', function () {
+
+			var returnedObject = backlog.remove(rsvp.defer().promise);
+
+			returnedObject.should.equal(backlog);
+
+		});
+
+	});
+
+	describe('#whenClear()', function () {
+
+		// Start from scratch just in case there's anything weird going on with the previous tests
+
+		backlog = rsvpBacklog();
+		deferreds = [];
+
+		it('should return a resolved promise when the backlog is empty', function (done) {
+
+			var clearPromise = backlog.whenClear();
+
+			clearPromise.should.be.Promise;
+
+			clearPromise.then(done);
+
+		});
+
+		it('should return a promise that resolves when the corresponding deferral does', function (done) {
+
+			deferreds.push(rsvp.defer());
+
+			deferreds[0].promise.then(done);
+
+			setTimeout(deferreds[0].resolve, 100);
+
+		});
+
+		it('should return a promise that resolves when a backlog > 1 is cleared', function (done) {
+
+			var iteration,
+				clearInterval;
+
+			backlog = rsvpBacklog();
+
+			for (iteration = 0; iteration < 100; iteration += 1) {
+
+				deferreds.push(rsvp.defer());
+				backlog.add(deferreds[deferreds.length - 1].promise);
+
+			}
+
+			backlog.whenClear().then(done);
+
+			clearInterval = setInterval(function () {
+
+				var poppedDeferral = deferreds.pop();
+
+				if (poppedDeferral) {
+
+					poppedDeferral.resolve();
+
+				}
+
+				else {
+
+					clearInterval();
+
+				}
+
+			}, 1);
+
+		});
+
+	});
+
+});
